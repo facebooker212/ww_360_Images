0b49d4726c3800895d0a10acd410bc6ec2e54b94
facebooker212 <facebooker3124@yahoo.com>
2023-06-03 21:11:18 -0600

    Added first car images

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/car1/image_<int:image_number>.png')
+@app.route('/car1/image_<int:image_number>.jpg')
 def serve_car1(image_number):
-    return send_from_directory('./car1', f'image_{image_number}.png')
+    return send_from_directory('./car1', f'image_{image_number}.jpg')
 
 @app.route('/car2/image_<int:image_number>.png')
 def serve_car2(image_number):
